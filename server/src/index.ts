ee34e291e8bc94f5b5d68040d5f951dc58051d82
xhrix <xxxhrixxx@gmail.com>
2017-08-17 19:23:41 -0500

    Simple node server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log("im server");
+import * as http from 'http';
+import * as socketIo from 'socket.io';
+import * as fs from 'fs';
+import {IncomingMessage, ServerResponse} from "http";
+
+const app = http.createServer(handler);
+const io = socketIo(app);
+
+app.listen(8000);
+console.log('listening on port 8000');
+
+function handler(req: IncomingMessage, res: ServerResponse) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.on('connection', function (socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+console.log('sockets were set up');
