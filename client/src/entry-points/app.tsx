c692b7cc2c78f7dc60eb056dea7896a33681e3b4
xhrix <xxxhrixxx@gmail.com>
2017-08-18 10:58:22 -0500

    simple reacct render

--- client/src/entry-points/app.tsx
+++ client/src/entry-points/app.tsx
@@ -1,1 +1,10 @@
-console.log('im the app entry point');
+import * as React from 'react'
+import * as ReactDOM from "react-dom"
+
+const appElement = document.getElementById('app');
+
+if (!appElement) {
+    throw new Error('No #app element.');
+}
+
+ReactDOM.render(<div>Soy react >:v</div>, appElement);
