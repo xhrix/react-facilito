99a52d165a0b0d6e24aa2e300f17b549dd4d9a8b
xhrix <xxxhrixxx@gmail.com>
2017-08-18 13:24:07 -0500

    Add: Create new task feature

--- client/src/web/components/screens/TasksScreen.tsx
+++ client/src/web/components/screens/TasksScreen.tsx
@@ -3,6 +3,8 @@ import {observer} from "mobx-react";
 import {observable} from "mobx";
 import todoTask1 from "../../../api-sdk/mocks/todo-task/todo-task-1";
 import todoTask2 from "../../../api-sdk/mocks/todo-task/todo-task-2";
+import {SyntheticEvent} from "react";
+import TodoTask from "../../../api-sdk/models/TodoTask";
 
 export interface TasksScreenProps {
 
@@ -19,6 +21,25 @@ export default class TasksScreen extends React.Component<TasksScreenProps, Tasks
     @observable
     private tasks = [todoTask1, todoTask2];
 
+    @observable
+    private newTaskText = '';
+
+    private onSubmitTask(event?: SyntheticEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if(this.newTaskText === '') {
+            alert('Escribe un texto');
+            return;
+        }
+
+        const newTask = new TodoTask();
+        newTask.id = Date.now();
+        newTask.content = this.newTaskText;
+        newTask.isCompleted = false;
+
+        this.newTaskText = '';
+        this.tasks.push(newTask);
+    }
+
     render() {
         return (
             <div>
@@ -26,11 +47,16 @@ export default class TasksScreen extends React.Component<TasksScreenProps, Tasks
                 <ul>
                     {this.tasks.map(x => (
                         <li key={`story${x.id}`}>
-                            <input type="checkbox" checked={x.isCompleted}/>
+                            <input type="checkbox" checked={x.isCompleted} onChange={event => null}/>
                             <span>{x.content}</span>
                         </li>
                     ))}
                 </ul>
+
+                <form onSubmit={this.onSubmitTask.bind(this)}>
+                    <input type="text" placeholder="Nueva tarea" value={this.newTaskText} onChange={e => this.newTaskText = e.target.value}/>
+                    <button type="submit">Agregar</button>
+                </form>
             </div>
         );
     }
