058bcbaf62180cda0850817c356f411302c98239
xhrix <xxxhrixxx@gmail.com>
2017-08-18 11:07:32 -0500

    Integrated simple sample with redux

--- client/src/web/components/screens/MainScreen.tsx
+++ client/src/web/components/screens/MainScreen.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import {observer} from "mobx-react";
+import {observable} from "mobx";
 
 export interface MainScreenProps {
 
@@ -8,10 +10,19 @@ export interface MainScreenState {
 
 }
 
+@observer
 export default class MainScreen extends React.Component<MainScreenProps, MainScreenState> {
+
+    @observable
+    private count = 0;
+
+    componentDidMount() {
+        setInterval(() => ++this.count, 200);
+    }
+
     render() {
         return (
-            <div>I'm the main screen component.</div>
+            <div>I'm the main screen component ({this.count}).</div>
         );
     }
 }
