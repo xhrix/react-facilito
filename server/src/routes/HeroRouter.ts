ff9cd7b647e6fdaf434f3734816fdd275172f72b
xhrix <xxxhrixxx@gmail.com>
2017-08-23 13:50:42 -0500

    Add heroes router

---
+++ server/src/routes/HeroRouter.ts
@@ -0,0 +1,35 @@
+import {Router, Request, Response, NextFunction} from 'express';
+
+const Heroes = require('../data');
+
+class HeroRouter {
+    router: Router;
+
+    /**
+     * Initialize the HeroRouter
+     */
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
+
+    /**
+     * GET all Heroes.
+     */
+    public static getAll(req: Request, res: Response, next: NextFunction) {
+        res.send(Heroes);
+    }
+
+    /**
+     * Take each handler, and attach to one of the Express.Router's
+     * endpoints.
+     */
+    private init() {
+        this.router.get('/', HeroRouter.getAll);
+    }
+}
+
+// Create the HeroRouter, and export its configured Express.Router
+const heroRoutes = new HeroRouter();
+
+export default heroRoutes.router;
