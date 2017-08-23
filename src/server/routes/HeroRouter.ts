f32c007f0cbec0052cf68c30d262c85b7e757b87
xhrix <xxxhrixxx@gmail.com>
2017-08-23 16:48:20 -0500

    add get by id endpoint

--- src/server/routes/HeroRouter.ts
+++ src/server/routes/HeroRouter.ts
@@ -1,6 +1,6 @@
 import {Router, Request, Response, NextFunction} from 'express';
 
-const Heroes = require('../assets/data');
+const Heroes = require('../assets/data') as any[];
 
 class HeroRouter {
     router: Router;
@@ -26,6 +26,30 @@ class HeroRouter {
      */
     private init() {
         this.router.get('/', HeroRouter.getAll);
+        this.router.get('/:id', this.getOne);
+    }
+
+    /**
+     * GET one hero by id
+     */
+    public getOne(req: Request, res: Response, next: NextFunction) {
+        let query = parseInt(req.params.id);
+        let hero = Heroes.find(hero => hero.id === query);
+        if (hero) {
+            res.status(200)
+                .send({
+                    message: 'Success',
+                    status: res.status,
+                    hero
+                });
+        }
+        else {
+            res.status(404)
+                .send({
+                    message: 'No hero found with the given id.',
+                    status: res.status
+                });
+        }
     }
 }
 
