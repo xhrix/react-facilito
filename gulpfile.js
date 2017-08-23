fb8d86817be6dac8ee0335be5ac077a381e1b3d5
xhrix <xxxhrixxx@gmail.com>
2017-08-23 11:53:15 -0500

    added gulp file to compile TS of server

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+
+gulp.task('scripts', function () {
+    const tsProject = ts.createProject('server/tsconfig.json');
+    const tsResult = tsProject.src().pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('server/dist'));
+});
+
+gulp.task('watch', ['scripts'], function () {
+    gulp.watch('server/src/**/*.ts', ['scripts']);
+});
+
+gulp.task('assets', function () {
+    return gulp.src(JSON_FILES).pipe(gulp.dest('server/dist'));
+});
+
+gulp.task('default', ['watch', 'assets']);
